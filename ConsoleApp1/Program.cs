93f71a247218c7e985dad3907da1c2e6f8afdc8c
dejan <dejan@DESKTOP-HA22HU0>
2023-02-28 18:39:55 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿var karten = new Stack<string>();
+karten.Push("Herz 2");
+karten.Push("Karo 3");
+karten.Push("Pik 10");
+karten.Push("Pik 5");
+karten.Push("Kreuz Dame");
+
+foreach (var x in karten)
+{
+    Console.WriteLine($"Oben steht {x}");
+}
+
+for (int i = karten.Count; i > 0; i--)
+    if (karten.Peek().Equals("Pik 10"))
+    {
+        break;
+    }
+    else
+    {
+        karten.Pop();
+    }
+
+Console.WriteLine($"\n---------- Die restlichen Karten----------");
+
+foreach (var x in karten)
+{
+    Console.WriteLine($"Oben steht {x}");
+}
+
